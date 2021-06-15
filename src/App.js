a1b7aacf7fb40d34c28dc5279acaf43213c233e6
rkkamboj <54266445+rkkamboj@users.noreply.github.com>
2021-06-15 18:09:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
+
 
 
 
@@ -11,14 +11,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> <Link to="/Contact">Homepage</Link>
+          <h1 className="App-title">Welcome to React</h1> 
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-		 <p className="App-intro">
-		 <Link to='/contact'>Contact Us</Link>
-		 </p>
+       
+			
       </div>
     );
   }
