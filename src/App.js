0b0993320ab0b59b02788c109fb51e6e0e07700a
rkkamboj <54266445+rkkamboj@users.noreply.github.com>
2021-06-15 19:00:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';	
 
 
 class App extends Component {
   render() {
-    return (
+	  
+	  const responseFacebook = (response) => {
+      console.log(response);
+    }
+	
+	const responseGoogle = (response) => {
+      console.log(response);
+    }
+	
+return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> 
-        </header>
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+      <FacebookLogin				
+        appId="124167923125048" //APP ID NOT CREATED YET
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+      <br />
+      <br />
+	   <GoogleLogin
+        clientId="135899586465-ogak40r1goijijsbii2upjoim8gkf05d.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
        
 			
       </div>
