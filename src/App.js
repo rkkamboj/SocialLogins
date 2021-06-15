533cbcd9c811bc73ca1fb89486899cc8b886f85b
rkkamboj <rkkamboj@gmail.com>
2021-06-15 07:22:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1> <Link to="/Contact">Homepage</Link>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+		 <p className="App-intro">
+		 <Link to='/contact'>Contact Us</Link>
+		 </p>
+      </div>
+    );
+  }
+}
+
+export default App;
